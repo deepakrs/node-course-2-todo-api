011a59d4e23f819e5fc33f355373aff61784f7a3
deepak <shindedeepak159@gmail.com>
2019-02-20 18:01:56 -0500

    Add POST /todos and refactor mongoose

---
+++ server/models/user.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+
+// User
+// email - require it - trim it - set type - set min length of 1
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
+});
+
+module.exports = {User};
+
+// var user = new User({
+//   email: 'andrew@example.com   '
+// });
+//
+// user.save().then((doc) => {
+//   console.log('User saved', doc);
+// }, (e) => {
+//   console.log('Unable to save user', e);
+// });
