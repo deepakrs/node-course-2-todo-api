0882e3c7ddad182ea04e95f9505e4a3e5dde8f77
deepak <shindedeepak159@gmail.com>
2019-02-20 22:39:28 -0500

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
@@ -27,6 +28,25 @@ app.get('/todos', (request, response) => {
     response.send({todos});
   }, (e) => {
     response.status(400).send(e);
+  });
+});
+
+// GET /todos/123456
+app.get('/todos/:id', (request, response) => {
+  var id = request.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return response.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return response.status(404).send();
+    }
+
+    response.send({todo});
+  }).catch((e) => {
+    response.status(400).send();
   });
 });
 
